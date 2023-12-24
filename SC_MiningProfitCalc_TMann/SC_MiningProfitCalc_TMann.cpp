a7e574eb9353cac2fa7da9af9ff9fd800750619b
Tr-Mann <150646157+Tr-Mann@users.noreply.github.com>
2023-12-24 00:09:53 -0500

    development start

--- SC_MiningProfitCalc_TMann/SC_MiningProfitCalc_TMann.cpp
+++ SC_MiningProfitCalc_TMann/SC_MiningProfitCalc_TMann.cpp
@@ -9,35 +9,44 @@
 using namespace std;
 
 void mainMenu();
-bool runCheck(int);
+bool runCheck();
 int validInput(int);
-struct minerals
-{
-	string name;
-	double massToSCU;
-	int price;
-};
+void display();
 int main()
 {
 	bool isRunning = true;
+	int input;
 
+	while (isRunning)
+	{
+		mainMenu();
+		input = validInput(2);
+		if (input == 0)
+		{
+			isRunning = runCheck();
+		}
+		else if (input == 1)
+		{
+			//Rock to price menu
+		}
+		else
+		{
+			//Load profit menu
+		}
+	}
 }
 
 void mainMenu()
 {
 	int input;
+
+	system("CLS");
+
 	cout << "| Mining Calculator |" << endl 
 		<< "|___________________|" << endl
 		<< "| 1. Rock to price  |" << endl
 		<< "| 2. Load Profit    |" << endl
 		<< "|___________________|" << endl;
-	input = validInput(2);
-	//If input is 0 runcheck will return false
-	runCheck(input);
-	if (input == 0)
-	{
-		
-	}
 }
 
 int validInput(int max)
@@ -53,9 +62,19 @@ int validInput(int max)
 	}
 	return input;
 }
-bool runCheck(int num)
+//This function will check if the user wants to continue running the program.
+bool runCheck()
 {
-	if (num == 0)
+	string input;
+	system("CLS");
+	cout << "Do you wish to quit program? (y/n)" << endl;
+	cin >> input;
+	while (input != "y" && input != "n")
+	{
+		cout << "Invalid input please try again (y/n)" << endl;
+		cin >> input;
+	}
+	if (input == "y")
 	{
 		return false;
 	}
@@ -63,4 +82,9 @@ bool runCheck(int num)
 	{
 		return true;
 	}
+}
+//This fucntion will display output of numbers from other fucntions eventually
+void display()
+{
+
 }
