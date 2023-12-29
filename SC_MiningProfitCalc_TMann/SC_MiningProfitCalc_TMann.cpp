f13e20e5c9d9554102f2e18baf486bcd9abbe00f
Tr-Mann <150646157+Tr-Mann@users.noreply.github.com>
2023-12-28 22:36:35 -0500

    added rock to profit

--- SC_MiningProfitCalc_TMann/SC_MiningProfitCalc_TMann.cpp
+++ SC_MiningProfitCalc_TMann/SC_MiningProfitCalc_TMann.cpp
@@ -8,10 +8,19 @@
 #include <iomanip>
 using namespace std;
 
+
+struct mineral
+{
+	string name;
+	int value;
+	double massToScu;
+};
+
+void rockToProf(mineral);
+mineral getMin(mineral[]);
 void mainMenu();
 bool runCheck();
 int validInput(int);
-void display();
 int main()
 {
 	bool isRunning = true;
@@ -19,8 +28,18 @@ int main()
 
 	while (isRunning)
 	{
+		mineral ores[10];
+		ores[0] = { "Quatanium", 23700, 682};
+		ores[1] = { "Bexalite", 7981, 231};
+		ores[2] = { "Gold", 7484, 644};
+		ores[3] = { "Taranite", 7686, 340};
+		ores[4] = { "Agricium", 2579, 240};
+		ores[5] = {"Beryl", 2710, 92};
+		ores[6] = {"Borase", 3532,  150};
+		ores[7] = {"Hephestanite", 2668, 107};
+		ores[8] = { "Laranite", 2887, 384 };
 		mainMenu();
-		input = validInput(2);
+		input = validInput(3);
 		if (input == 0)
 		{
 			isRunning = runCheck();
@@ -28,24 +47,28 @@ int main()
 		else if (input == 1)
 		{
 			//Rock to price menu
+			rockToProf(getMin(ores));
+		}
+		else if (input == 2)
+		{
+			//Load profit menu
 		}
 		else
 		{
-			//Load profit menu
+			//mineral list
 		}
 	}
 }
 
 void mainMenu()
 {
-	int input;
-
 	system("CLS");
 
 	cout << "| Mining Calculator |" << endl 
 		<< "|___________________|" << endl
 		<< "| 1. Rock to price  |" << endl
 		<< "| 2. Load Profit    |" << endl
+		<< "| 3. Mineral List   |" << endl
 		<< "|___________________|" << endl;
 }
 
@@ -83,8 +106,69 @@ bool runCheck()
 		return true;
 	}
 }
-//This fucntion will display output of numbers from other fucntions eventually
-void display()
+mineral getMin(mineral ores[])
 {
+	int input;
 
+	system("CLS");
+	cout << "What mineral are you mining?" << endl
+		<< "1. Quatanium" << endl
+		<< "2. Bexalite" << endl
+		<< "3. Gold" << endl
+		<< "4. Taranite" << endl
+		<< "5. Agricium" << endl
+		<< "6. Beryl" << endl
+		<< "7. Borase" << endl
+		<< "8. Hephestanite" << endl
+		<< "9. Laranite" << endl;
+	cin >> input;
+	while (input < 1 || input > 9)
+	{
+		cout << "Invalid input try again." << endl;
+		cin >> input;
+	}
+
+	switch (input)
+	{
+	case 1: return ores[0];
+		break;
+	case 2: return ores[1];
+		break;
+	case 3: return ores[2];
+		break;
+	case 4: return ores[3];
+		break;
+	case 5: return ores[4];
+		break;
+	case 6: return ores[5];
+		break;
+	case 7: return ores[6];
+		break;
+	case 8: return ores[7];
+		break;
+	case 9: return ores[8];
+		break;
+	}
+}
+void rockToProf(mineral ore)
+{
+	int input;
+	double profit;
+	double scus;
+	double mass;
+	double percentage;
+	cout << "Mining: " << ore.name << " what is the mass of the rock you are mining?" << endl;
+	cin >> mass;
+	cout << "What percentage " << ore.name << " is the " << mass << " mass rock you are mining?" << endl;
+	cin >> percentage;
+
+	percentage = percentage / 100;
+
+	scus = ((mass * percentage) / ore.massToScu);
+	profit = scus * ore.value;
+	
+	cout << "SCUS minable: " << scus << endl
+		<< "Profits: " << profit << " auec" << endl
+		<< "Type 0 to return to main menu.";
+	cin >> input;
 }
